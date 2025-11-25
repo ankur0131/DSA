3711fc18bc7769c75e27601ec7e66a8fb80f861d
ankur0131 <ankur10312002@gmail.com>
2025-11-26 01:33:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (22.56%) - LeetSync

---
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if(k%2==0 || k%5==0){
+            return -1;
+        }
+        int rem = 0;
+        int length;
+        for(length = 1 ; length<=k; length++){
+            rem = (rem*10+1)%k;
+            if(rem==0)return length;
+        }
+        return -1;
+    }
+};
