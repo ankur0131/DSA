650df77b8116e6d0abb053ffab38614ab3d50762
ankur0131 <ankur10312002@gmail.com>
2025-12-08 18:30:38 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (80.92%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int cnt1=0;
+        int cnt2=0;
+        int el1=INT_MIN;
+        int el2=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(cnt1==0&&el2!=nums[i]){
+                cnt1=1;
+                el1=nums[i];
+
+            }
+            else if(cnt2==0&&el1!=nums[i]){
+                cnt2=1;
+                el2=nums[i];
+            }
+            else if(nums[i]==el1){
+                cnt1++;
+            }
+            else if(nums[i]==el2){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        vector<int> ls;
+        cnt1=0;
+        cnt2=0;
+        int n=(int)(nums.size()/3)+1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==el1){
+                cnt1++;
+            }
+            if(nums[i]==el2){
+                cnt2++;
+            }
+        }
+        if(cnt1>=n){
+            ls.push_back(el1);
+        }
+        if(cnt2>=n){
+            ls.push_back(el2);
+        }
+        return ls;
+        
+    }
+};
